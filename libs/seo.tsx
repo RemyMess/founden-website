698bd245bbe4b9ce27c9e515c1b52947eea5924d
remymess <remy.messadene@gmail.com>
2024-05-29 19:03:07 +0100

    metadata image

--- libs/seo.tsx
+++ libs/seo.tsx
@@ -89,7 +89,7 @@ export const renderSchemaTags = () => {
           url: `https://${config.domainName}/`,
           author: {
             "@type": "Person",
-            name: "Marc Lou",
+            name: "Remy Messadene",
           },
           datePublished: "2023-08-01",
           applicationCategory: "EducationalApplication",
